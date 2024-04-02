849a7671396bd1b3c11bc93c5c550e9a8bb72fb4
javad <javadht1990a@gmail.com>
2024-04-02 23:23:21 +0330

    Add the primary table

--- src/pages/orders.jsx
+++ src/pages/orders.jsx
@@ -1,11 +1,13 @@
 import OrderHeader from "../components/ordersHeader/ordersHeader"
 import { Box } from "@mui/material"
+import EnhancedTable from "../components/orderTable/orderTable"
 
 const Orders = () => {
     return (
 
         <Box component="main" sx={{ flexGrow: 1, p: 3, display: "flex", flexDirection: "column" }}>
             <OrderHeader />
+            <EnhancedTable />
         </Box>
 
     )
