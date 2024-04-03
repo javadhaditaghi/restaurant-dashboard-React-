2715495790d07731291bb162dabaaf0cb186d2fc
javad <javadht1990a@gmail.com>
2024-04-04 00:47:51 +0330

    Modify some stylings

--- src/components/orderTable/orderTable.jsx
+++ src/components/orderTable/orderTable.jsx
@@ -253,13 +253,27 @@ export default function EnhancedTable() {
             onClose={handleMenuClose}
 
         >
-            <MenuItem onClick={handleMenuClose} sx={{ padding: "15px 20px" }}>
+            <MenuItem onClick={handleMenuClose} sx={{
+                padding: "15px 20px",
+                transition: "color 0.2s ease, font-weight 0.2s ease",
+                '&:hover': {
+                    color: (theme) => theme.palette.primary.main,
+                    fontWeight: "600"
+                }
+            }}>
                 <Box sx={{ pr: 2, display: "flex", }}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                     <path d="M22 11.08V12C21.9988 14.1564 21.3005 16.2547 20.0093 17.9818C18.7182 19.709 16.9033 20.9725 14.8354 21.5839C12.7674 22.1953 10.5573 22.1219 8.53447 21.3746C6.51168 20.6273 4.78465 19.2461 3.61096 17.4371C2.43727 15.628 1.87979 13.4881 2.02168 11.3363C2.16356 9.18455 2.99721 7.13631 4.39828 5.49706C5.79935 3.85781 7.69279 2.71537 9.79619 2.24013C11.8996 1.7649 14.1003 1.98232 16.07 2.85999" stroke="#2F4CDD" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                     <path d="M22 4L12 14.01L9 11.01" stroke="#2F4CDD" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                 </svg></Box>
                 Accept Order</MenuItem>
-            <MenuItem onClick={handleMenuClose} sx={{ padding: "15px 20px" }}>
+            <MenuItem onClick={handleMenuClose} sx={{
+                padding: "15px 20px",
+                transition: "color 0.2s ease, font-weight 0.2s ease",
+                '&:hover': {
+                    color: (theme) => theme.palette.danger.main,
+                    fontWeight: "600"
+                }
+            }}>
                 <Box sx={{ pr: 2, display: "flex", }}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                     <path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" stroke="#F24242" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                     <path d="M15 9L9 15" stroke="#F24242" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
@@ -328,14 +342,20 @@ export default function EnhancedTable() {
     );
 
     return (
-        <Box sx={{ width: '100%' }}>
-            <Paper sx={{ width: '100%', mb: 2 }}>
+        <Box sx={{
+            width: '100%', [`& .css-1mxz8qt-MuiPaper-root`]: {
+                borderRadius: "15px"
+            }
+        }}>
+            <Paper sx={{ width: '100%', mb: 2, }}>
 
-                <TableContainer>
+                <TableContainer >
                     <Table
                         sx={{
                             minWidth: 750,
-
+                            [`& .css-1mxz8qt-MuiPaper-root`]: {
+                                borderRadius: "15px"
+                            }
                         }}
                         aria-labelledby="tableTitle"
                         size={dense ? 'small' : 'medium'}
