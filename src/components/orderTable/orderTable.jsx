d6e228c54e9ca414a506b37ed78f29a172d06962
javad <javadht1990a@gmail.com>
2024-04-03 17:58:48 +0330

    Add status and help columns to the end of the table

--- src/components/orderTable/orderTable.jsx
+++ src/components/orderTable/orderTable.jsx
@@ -22,7 +22,7 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import FilterListIcon from '@mui/icons-material/FilterList';
 import { visuallyHidden } from '@mui/utils';
 
-function createData(id, name, calories, fat, carbs, protein) {
+function createData(id, name, calories, fat, carbs, protein, status, help) {
     return {
         id,
         name,
@@ -30,23 +30,26 @@ function createData(id, name, calories, fat, carbs, protein) {
         fat,
         carbs,
         protein,
+        status,
+        help
+
     };
 }
 
 const rows = [
-    createData(1, 'Cupcake', 305, 3.7, 67, 4.3),
-    createData(2, 'Donut', 452, 25.0, 51, 4.9),
-    createData(3, 'Eclair', 262, 16.0, 24, 6.0),
-    createData(4, 'Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData(5, 'Gingerbread', 356, 16.0, 49, 3.9),
-    createData(6, 'Honeycomb', 408, 3.2, 87, 6.5),
+    createData(1, 'Cupcake', 305, 3.7, 67, 4.3, "available"),
+    createData(2, 'Donut', 452, 25.0, 51, 4.9, "Not available"),
+    createData(3, 'Eclair', 262, 16.0, 24, 6.0, "available"),
+    createData(4, 'Frozen yoghurt', 159, 6.0, 24, 4.0, "available"),
+    createData(5, 'Gingerbread', 356, 16.0, 49, 3.9, "Not available"),
+    createData(6, 'Honeycomb', 408, 3.2, 87, 6.5, "available"),
     createData(7, 'Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData(8, 'Jelly Bean', 375, 0.0, 94, 0.0),
-    createData(9, 'KitKat', 518, 26.0, 65, 7.0),
-    createData(10, 'Lollipop', 392, 0.2, 98, 0.0),
-    createData(11, 'Marshmallow', 318, 0, 81, 2.0),
-    createData(12, 'Nougat', 360, 19.0, 9, 37.0),
-    createData(13, 'Oreo', 437, 18.0, 63, 4.0),
+    createData(8, 'Jelly Bean', 375, 0.0, 94, 0.0, "Not available"),
+    createData(9, 'KitKat', 518, 26.0, 65, 7.0, "available"),
+    createData(10, 'Lollipop', 392, 0.2, 98, 0.0, "available"),
+    createData(11, 'Marshmallow', 318, 0, 81, 2.0, "Not available"),
+    createData(12, 'Nougat', 360, 19.0, 9, 37.0, "available"),
+    createData(13, 'Oreo', 437, 18.0, 63, 4.0, "Not available"),
 ];
 
 function descendingComparator(a, b, orderBy) {
@@ -86,31 +89,43 @@ const headCells = [
         id: 'name',
         numeric: false,
         disablePadding: true,
-        label: 'Dessert (100g serving)',
+        label: 'Order ID',
     },
     {
         id: 'calories',
         numeric: true,
         disablePadding: false,
-        label: 'Calories',
+        label: 'Date',
     },
     {
         id: 'fat',
         numeric: true,
         disablePadding: false,
-        label: 'Fat (g)',
+        label: 'Customer Name',
     },
     {
         id: 'carbs',
         numeric: true,
         disablePadding: false,
-        label: 'Carbs (g)',
+        label: 'Location',
     },
     {
         id: 'protein',
         numeric: true,
         disablePadding: false,
-        label: 'Protein (g)',
+        label: 'Amount',
+    },
+    {
+        id: 'status',
+        numeric: true,
+        disablePadding: false,
+        label: 'Status',
+    },
+    {
+        id: 'help',
+        numeric: true,
+        disablePadding: false,
+        label: ' ',
     },
 ];
 
@@ -350,6 +365,8 @@ export default function EnhancedTable() {
                                         <TableCell align="right">{row.fat}</TableCell>
                                         <TableCell align="right">{row.carbs}</TableCell>
                                         <TableCell align="right">{row.protein}</TableCell>
+                                        <TableCell align="right">{row.status}</TableCell>
+                                        <TableCell align="right">{row.help}</TableCell>
                                     </TableRow>
                                 );
                             })}
