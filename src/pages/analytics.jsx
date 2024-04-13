565883891306cc87f931b3093b612c4c02f73be8
javad <javadht1990a@gmail.com>
2024-04-13 20:55:59 +0330

    Add the header to analytics section

--- src/pages/analytics.jsx
+++ src/pages/analytics.jsx
@@ -1,10 +1,16 @@
 import { Box } from "@mui/material"
+import Title from "../components/dashboardHeader/title"
+import SelectData from "../components/dashboardHeader/select"
 
 
 const Analytics = () => {
     return (
-        <Box>
-            This is a test
+        <Box component="main" sx={{ flexGrow: 1, p: 3, display: "flex", flexDirection: "column" }}>
+            <Box sx={{ width: "100%", display: "flex", justifyContent: "space-between", py: 3, alignItems: "center" }}>
+                <Title title="Analytics" subtitle="Here is your restaurant summary with graph view" />
+                <SelectData />
+            </Box>
+
         </Box>
     )
 }
