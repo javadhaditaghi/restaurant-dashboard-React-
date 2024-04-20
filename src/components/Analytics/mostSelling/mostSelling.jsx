405c029ccb435b8e0843ec7d2028cf603d4675a0
javad <javadht1990a@gmail.com>
2024-04-20 19:11:33 +0330

    Add the interactive features for daily, monthly, weekly

--- src/components/Analytics/mostSelling/mostSelling.jsx
+++ src/components/Analytics/mostSelling/mostSelling.jsx
@@ -6,19 +6,32 @@ import Grid2 from "@mui/material/Unstable_Grid2/Grid2"
 import Avatar from "@mui/material/Avatar"
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import IconButton from '@mui/material/IconButton';
-import jsonData from './mostSellingItems.json'
+import jsonData from './mostSellingItems.json';
+import { useState } from "react"
 
 
 const MostSellingItems = () => {
+    const dilyData = jsonData.daily
+    const weeklyData = jsonData.weekly
+    const monthlyData = jsonData.monthly
+
+    // Differentiationg between daily, weekly and monthly
+    const [data, setData] = useState(dilyData)
+
+    const onBottonClick = (index) => {
+        setData(index == 2 ? dilyData : (index == 1 ? weeklyData : monthlyData))
+    }
+
+
     return (
         <InnerBox2>
             <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", padding: "10px" }}>
                 <CardHeaderTxt title="Most Selling Items" subtitle="Lorem ipsum dolor sit amet, consectetur" />
-                <OrderInfHeader />
+                <OrderInfHeader onButtonClick={onBottonClick} />
             </Box>
 
 
-            {jsonData.map((item, index) => (
+            {data.map((item, index) => (
                 <Grid2 container rowSpacing={2} columnSpacing={{ xs: 1, sm: 2, md: 3 }} sx={{ pt: 4 }} key={index}>
 
                     <Grid2 md={1}></Grid2>
